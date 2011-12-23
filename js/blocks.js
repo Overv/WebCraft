d2265149ea1a5f6cdb556756eab0c46a6e0a2791
Alexander Overvoorde <overv161@gmail.com>
2011-12-23 20:26:17 +0100

    Adds basic fluid physics with lava as sample material and adds a step system to the physics simulation.

--- js/blocks.js
+++ js/blocks.js
@@ -17,14 +17,15 @@ BLOCK = {};
 
 // Air
 BLOCK.AIR = {
-	transparent: true,
-	gravity: false
+	transparent: true
 };
 
 // Dirt
 BLOCK.DIRT = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir )
 	{
 		if ( dir == DIRECTION.UP && lit )
@@ -39,7 +40,9 @@ BLOCK.DIRT = {
 // Wood
 BLOCK.WOOD = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir )
 	{
 		if ( dir == DIRECTION.UP || dir == DIRECTION.DOWN )
@@ -52,7 +55,9 @@ BLOCK.WOOD = {
 // TNT
 BLOCK.TNT = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir )
 	{
 		if ( dir == DIRECTION.UP || dir == DIRECTION.DOWN )
@@ -65,7 +70,9 @@ BLOCK.TNT = {
 // Bookcase
 BLOCK.BOOKCASE = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir )
 	{
 		if ( dir == DIRECTION.FORWARD || dir == DIRECTION.BACK )
@@ -75,87 +82,123 @@ BLOCK.BOOKCASE = {
 	}
 };
 
+// Lava
+BLOCK.LAVA = {
+	transparent: false,
+	selflit: true,
+	gravity: true,
+	fluid: true,
+	texture: function( world, lightmap, lit, x, y, z, dir )
+	{
+		return [ 13/16, 14/16, 14/16, 15/16 ];
+	}
+};
+
 // Plank
 BLOCK.PLANK = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 4/16, 0/16, 5/16, 1/16 ]; }
 };
 
 // Cobblestone
 BLOCK.COBBLESTONE = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 0/16, 1/16, 1/16, 2/16 ]; }
 };
 
 // Concrete
 BLOCK.CONCRETE = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 1/16, 0/16, 2/16, 1/16 ]; }
 };
 
 // Brick
 BLOCK.BRICK = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 7/16, 0/16, 8/16, 1/16 ]; }
 };
 
 // Sand
 BLOCK.SAND = {
 	transparent: false,
+	selflit: false,
 	gravity: true,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 2/16, 1/16, 3/16, 2/16 ]; }
 };
 
 // Gravel
 BLOCK.GRAVEL = {
 	transparent: false,
+	selflit: false,
 	gravity: true,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 3/16, 1/16, 4/16, 2/16 ]; }
 };
 
 // Iron
 BLOCK.IRON = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 6/16, 1/16, 7/16, 2/16 ]; }
 };
 
 // Gold
 BLOCK.GOLD = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 7/16, 1/16, 8/16, 2/16 ]; }
 };
 
 // Diamond
 BLOCK.DIAMOND = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 8/16, 1/16, 9/16, 2/16 ]; }
 };
 
 // Obsidian
 BLOCK.OBSIDIAN = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 5/16, 2/16, 6/16, 3/16 ]; }
 };
 
 // Glass
 BLOCK.GLASS = {
 	transparent: true,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 1/16, 3/16, 2/16, 4/16 ]; }
 };
 
 // Sponge
 BLOCK.SPONGE = {
 	transparent: false,
+	selflit: false,
 	gravity: false,
+	fluid: false,
 	texture: function( world, lightmap, lit, x, y, z, dir ) { return [ 0/16, 3/16, 1/16, 4/16 ]; }
 };
 
@@ -168,13 +211,15 @@ BLOCK.pushVertices = function( vertices, world, lightmap, x, y, z )
 {
 	var blocks = world.blocks;
 	var blockLit = z >= lightmap[x][y];
+	var block = blocks[x][y][z];
 	
 	// Top
 	if ( z == world.sz - 1 || world.blocks[x][y][z+1].transparent )
 	{
-		var c = world.blocks[x][y][z].texture( world, lightmap, blockLit, x, y, z, DIRECTION.UP );
+		var c = block.texture( world, lightmap, blockLit, x, y, z, DIRECTION.UP );
 		
 		var lightMultiplier = z >= lightmap[x][y] ? 1.0 : 0.6;
+		if ( block.selflit ) lightMultiplier = 1.0;
 		
 		pushQuad(
 			vertices,
@@ -188,23 +233,26 @@ BLOCK.pushVertices = function( vertices, world, lightmap, x, y, z )
 	// Bottom
 	if ( z == 0 || world.blocks[x][y][z-1].transparent )
 	{
-		var c = world.blocks[x][y][z].texture( world, lightmap, blockLit, x, y, z, DIRECTION.DOWN );
+		var c = block.texture( world, lightmap, blockLit, x, y, z, DIRECTION.DOWN );
+		
+		var lightMultiplier = block.selflit ? 1.0 : 0.6;
 		
 		pushQuad(
 			vertices,							
-			[ x, y + 1.0, z, c[0], c[3], 0.6, 0.6, 0.6, 1.0 ],
-			[ x + 1.0, y + 1.0, z, c[2], c[3], 0.6, 0.6, 0.6, 1.0 ],
-			[ x + 1.0, y, z, c[2], c[1], 0.6, 0.6, 0.6, 1.0 ],
-			[ x, y, z, c[0], c[1], 0.6, 0.6, 0.6, 1.0 ]
+			[ x, y + 1.0, z, c[0], c[3], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ],
+			[ x + 1.0, y + 1.0, z, c[2], c[3], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ],
+			[ x + 1.0, y, z, c[2], c[1], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ],
+			[ x, y, z, c[0], c[1], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ]
 		);
 	}
 	
 	// Front
 	if ( y == 0 || world.blocks[x][y-1][z].transparent )
 	{
-		var c = world.blocks[x][y][z].texture( world, lightmap, blockLit, x, y, z, DIRECTION.FORWARD );
+		var c = block.texture( world, lightmap, blockLit, x, y, z, DIRECTION.FORWARD );
 		
 		var lightMultiplier = ( y == 0 || z >= lightmap[x][y-1] ) ? 1.0 : 0.6;
+		if ( block.selflit ) lightMultiplier = 1.0;
 		
 		pushQuad(
 			vertices,
@@ -218,37 +266,42 @@ BLOCK.pushVertices = function( vertices, world, lightmap, x, y, z )
 	// Back
 	if ( y == world.sy - 1 || world.blocks[x][y+1][z].transparent )
 	{
-		var c = world.blocks[x][y][z].texture( world, lightmap, blockLit, x, y, z, DIRECTION.BACK );
+		var c = block.texture( world, lightmap, blockLit, x, y, z, DIRECTION.BACK );
+		
+		var lightMultiplier = block.selflit ? 1.0 : 0.6;
 		
 		pushQuad(
 			vertices,
-			[ x, y + 1.0, z + 1.0, c[2], c[1], 0.6, 0.6, 0.6, 1.0 ],
-			[ x + 1.0, y + 1.0, z + 1.0, c[0], c[1], 0.6, 0.6, 0.6, 1.0 ],
-			[ x + 1.0, y + 1.0, z, c[0], c[3], 0.6, 0.6, 0.6, 1.0 ],
-			[ x, y + 1.0, z, c[2], c[3], 0.6, 0.6, 0.6, 1.0 ]
+			[ x, y + 1.0, z + 1.0, c[2], c[1], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ],
+			[ x + 1.0, y + 1.0, z + 1.0, c[0], c[1], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ],
+			[ x + 1.0, y + 1.0, z, c[0], c[3], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ],
+			[ x, y + 1.0, z, c[2], c[3], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ]
 		);
 	}
 	
 	// Left
 	if ( x == 0 || world.blocks[x-1][y][z].transparent )
 	{
-		var c = world.blocks[x][y][z].texture( world, lightmap, blockLit, x, y, z, DIRECTION.LEFT );
+		var c = block.texture( world, lightmap, blockLit, x, y, z, DIRECTION.LEFT );
+		
+		var lightMultiplier = block.selflit ? 1.0 : 0.6;
 		
 		pushQuad(
 			vertices,
-			[ x, y, z + 1.0, c[2], c[1], 0.6, 0.6, 0.6, 1.0 ],
-			[ x, y + 1.0, z + 1.0, c[0], c[1], 0.6, 0.6, 0.6, 1.0 ],
-			[ x, y + 1.0, z, c[0], c[3], 0.6, 0.6, 0.6, 1.0 ],
-			[ x, y, z, c[2], c[3], 0.6, 0.6, 0.6, 1.0 ]
+			[ x, y, z + 1.0, c[2], c[1], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ],
+			[ x, y + 1.0, z + 1.0, c[0], c[1], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ],
+			[ x, y + 1.0, z, c[0], c[3], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ],
+			[ x, y, z, c[2], c[3], lightMultiplier, lightMultiplier, lightMultiplier, 1.0 ]
 		);
 	}
 	
 	// Right
 	if ( x == world.sx - 1 || world.blocks[x+1][y][z].transparent )
 	{
-		var c = world.blocks[x][y][z].texture( world, lightmap, blockLit, x, y, z, DIRECTION.RIGHT );
+		var c = block.texture( world, lightmap, blockLit, x, y, z, DIRECTION.RIGHT );
 		
 		var lightMultiplier = ( x == world.sx - 1 || z >= lightmap[x+1][y] ) ? 1.0 : 0.6;
+		if ( block.selflit ) lightMultiplier = 1.0;
 		
 		pushQuad(
 			vertices,
--- js/physics.js
+++ js/physics.js
@@ -11,7 +11,7 @@
 
 function Physics()
 {
-	
+	this.lastStep = -1;
 }
 
 // setWorld( world )
@@ -33,14 +33,56 @@ Physics.prototype.simulate = function()
 	var world = this.world;
 	var blocks = world.blocks;
 	
+	var step = Math.floor( new Date().getTime() / 100 );
+	if ( step == this.lastStep ) return;
+	this.lastStep = step;
+	
 	// Gravity
-	for ( var x = 0; x < world.sx; x++ ) {
-		for ( var y = 0; y < world.sy; y++ ) {
-			for ( var z = 0; z < world.sz; z++ ) {
-				if ( blocks[x][y][z].gravity && z > 0 && blocks[x][y][z-1] == BLOCK.AIR )
-				{
-					world.setBlock( x, y, z - 1, blocks[x][y][z] );
-					world.setBlock( x, y, z, BLOCK.AIR );
+	if ( step % 1 == 0 )
+	{
+		for ( var x = 0; x < world.sx; x++ ) {
+			for ( var y = 0; y < world.sy; y++ ) {
+				for ( var z = 0; z < world.sz; z++ ) {
+					if ( blocks[x][y][z].gravity && z > 0 && blocks[x][y][z-1] == BLOCK.AIR )
+					{
+						world.setBlock( x, y, z - 1, blocks[x][y][z] );
+						world.setBlock( x, y, z, BLOCK.AIR );
+					}
+				}
+			}
+		}
+	}
+	
+	// Fluids
+	if ( step % 10 == 0 )
+	{
+		// Newly spawned fluid blocks are stored so that those aren't
+		// updated in the same step, creating a simulation avalanche.
+		var newFluidBlocks = {};
+		
+		for ( var x = 0; x < world.sx; x++ ) {
+			for ( var y = 0; y < world.sy; y++ ) {
+				for ( var z = 0; z < world.sz; z++ ) {
+					var material = blocks[x][y][z];
+					if ( material.fluid && newFluidBlocks[x+","+y+","+z] == null )
+					{
+						if ( x > 0 && blocks[x-1][y][z] == BLOCK.AIR ) {
+							world.setBlock( x - 1, y, z, material );
+							newFluidBlocks[(x-1)+","+y+","+z] = true;
+						}
+						if ( x < world.sx - 1 && blocks[x+1][y][z] == BLOCK.AIR ) {
+							world.setBlock( x + 1, y, z, material );
+							newFluidBlocks[(x+1)+","+y+","+z] = true;
+						}
+						if ( y > 0 && blocks[x][y-1][z] == BLOCK.AIR ) {
+							world.setBlock( x, y - 1, z, material );
+							newFluidBlocks[x+","+(y-1)+","+z] = true;
+						}
+						if ( y < world.sy - 1 && blocks[x][y+1][z] == BLOCK.AIR ) {
+							world.setBlock( x, y + 1, z, material );
+							newFluidBlocks[x+","+(y+1)+","+z] = true;
+						}
+					}
 				}
 			}
 		}
